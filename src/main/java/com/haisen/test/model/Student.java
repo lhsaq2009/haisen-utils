5ffd668af2088b3a32b04a8e8288ee18cb501874
haisen <lhsaq2009@163.com>
2022-04-04 17:39:59 +0800

    None

--- src/main/java/com/haisen/test/model/Student.java
+++ src/main/java/com/haisen/test/model/Student.java
@@ -2,7 +2,16 @@ package com.haisen.test.model;
 
 import com.google.common.collect.Lists;
 
+import com.haisen.common.Constant;
+
+import org.apache.commons.lang3.RandomStringUtils;
+import org.apache.commons.lang3.math.NumberUtils;
+import org.springframework.util.Assert;
+
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Random;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -23,12 +32,13 @@ public class Student {
     private String name;
 
     public static void main(String[] args) {
-        Student aa = new Student(1, "aa");
-        System.out.println(aa);
+        System.out.println(Student.get());
     }
 
     public static Student get() {
-        return Student.builder().id(1).name("java").build();
+        int size = Constant.personNames().size();
+        int i = new Random().nextInt(size);
+        return Student.builder().id(i).name(Constant.personNames().get(i)).build();
     }
 
     public static List<Student> list() {
@@ -40,5 +50,23 @@ public class Student {
         );
     }
 
+    public static List<Student> listRandom(int size) {
 
+        if (size < 0) {
+            return Collections.emptyList();
+        }
+
+        ArrayList<Student> result = Lists.newArrayListWithCapacity(size);
+
+        int namesSize = Constant.personNames().size();
+        for (int i = 0; i < size; i++) {
+            int id = new Random().nextInt(namesSize);
+            result.add(Student.builder()
+                    .id(id)
+                    .name(Constant.personNames().get(id))
+                    .build());
+        }
+
+        return result;
+    }
 }
