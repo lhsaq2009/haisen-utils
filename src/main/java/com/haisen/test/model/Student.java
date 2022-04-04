aa94cf375c510e8bfa6b830fefff918d2c88d09d
haisen <lhsaq2009@163.com>
2022-04-04 10:54:57 +0800

    None

---
+++ src/main/java/com/haisen/test/model/Student.java
@@ -0,0 +1,18 @@
+package com.haisen.test.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.experimental.SuperBuilder;
+
+/**
+ * <p>〈功能概述〉.
+ * @author haisen /20224/4 
+ */
+@Data
+@SuperBuilder
+@Builder
+@AllArgsConstructor
+public class Student /*extends Person*/ {
+
+}
