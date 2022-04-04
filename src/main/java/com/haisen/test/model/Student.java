0545816a8d7c472412c733f669b7114c306aac19
haisen <lhsaq2009@163.com>
2022-04-04 12:58:32 +0800

    None

--- src/main/java/com/haisen/test/model/Student.java
+++ src/main/java/com/haisen/test/model/Student.java
@@ -1,5 +1,9 @@
 package com.haisen.test.model;
 
+import com.google.common.collect.Lists;
+
+import java.util.List;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -22,4 +26,19 @@ public class Student {
         Student aa = new Student(1, "aa");
         System.out.println(aa);
     }
+
+    public static Student get() {
+        return Student.builder().id(1).name("java").build();
+    }
+
+    public static List<Student> list() {
+        int id = 1;
+        return Lists.newArrayList(
+                Student.builder().id(id++).name("java").build(),
+                Student.builder().id(id++).name("php").build(),
+                Student.builder().id(id++).name("c").build()
+        );
+    }
+
+
 }
