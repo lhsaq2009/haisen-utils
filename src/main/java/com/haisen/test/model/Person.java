ceaed266ae5db86eed15d273bf89ffb56e37e6cb
haisen <lhsaq2009@163.com>
2022-04-04 11:18:42 +0800

    None

--- src/main/java/com/haisen/test/model/Person.java
+++ src/main/java/com/haisen/test/model/Person.java
@@ -1,5 +1,6 @@
 package com.haisen.test.model;
 
+import lombok.Builder;
 import lombok.Data;
 
 /**
@@ -7,7 +8,13 @@ import lombok.Data;
  * @author haisen /20224/4 
  */
 @Data
+@Builder
 public class Person {
     private Integer id;
     private String name;
+
+    public static void main(String[] args) {
+        Person build = Person.builder().build();
+        System.out.println(build);
+    }
 }
