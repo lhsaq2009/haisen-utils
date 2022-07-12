60b6516383edb841d21533af71754fd7011a7af7
haisen <lhsaq2009@163.com>
2022-07-12 21:09:00 +0800

    新增自定义线程池

---
+++ src/main/java/com/haisen/common/utils/ThreadPoolUtils.java
@@ -0,0 +1,30 @@
+package com.haisen.common.utils;
+
+
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * <p>〈功能概述〉.
+ *
+ * @author haisen /20227/12
+ */
+public class ThreadPoolUtils {
+
+    public ExecutorService buildThreadPool(String threadName) {
+        ThreadFactory namedThreadFactory = new ThreadFactoryBuilder().setNameFormat(threadName + "-%d").build();
+        return new ThreadPoolExecutor(
+                5, 5,
+                0L, TimeUnit.MILLISECONDS,
+
+                new ArrayBlockingQueue<>(5),            // 阻塞队列 => 推荐使用有界队列，有界队列有助于避免资源耗尽的情况发生
+                namedThreadFactory,
+                new ThreadPoolExecutor.AbortPolicy());
+    }
+
+}
